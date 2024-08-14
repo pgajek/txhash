89434a51450bf24a2169177665613263350bf75d
pgajek <pgajek94@gmail.com>
2024-08-14 20:21:48 +0200

    working example

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ use url::Url;
 
 #[derive(Parser, Debug, Clone)]
 pub struct CliInput {
-    #[arg(long, short, env = "CONTRACT_ADDRESS")]
-    pub contract_address: String,
     #[arg(long, short, env = "TX_HASH")]
     pub tx_hash: String,
     #[arg(long, short, env = "RPC_URL")]
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 use alloy::{
     consensus::ReceiptEnvelope,
-    network::{EthereumWallet, TransactionBuilder},
-    node_bindings::Anvil,
-    primitives::{b256, FixedBytes},
-    providers::{ext::DebugApi, Provider, ProviderBuilder},
-    rpc::types::trace::geth::{
-        GethDebugBuiltInTracerType, GethDebugTracerType, GethDebugTracingOptions,
-        GethDefaultTracingOptions,
-    },
-    rpc::types::TransactionReceipt,
-    signers::local::PrivateKeySigner,
-    sol,
+    primitives::{b256, keccak256, Address, FixedBytes, Uint, U256},
+    providers::{Provider, ProviderBuilder},
 };
 
 use clap::Parser;
 use eyre::Result;
 use tx_event::CliInput;
 
-// sol!(
-//     #[allow(missing_docs)]
-//     #[sol(rpc)]
-//     IWETH9,
-//     "./abi.json"
-// );
 #[tokio::main]
 pub async fn main() -> Result<()> {
     let args = CliInput::parse();
@@ -35,27 +20,43 @@ pub async fn main() -> Result<()> {
 
     let receipt_option = provider.get_transaction_receipt(fixed_bytes_hash).await?;
 
-    // println!(
-    //     "***************RECEIPT****************: {:?}",
-    //     receipt_option
-    // );
-
     if let Some(receipt) = receipt_option {
         match receipt.inner {
             ReceiptEnvelope::Legacy(receipt_with_bloom) => {
                 let logs = receipt_with_bloom.receipt.logs;
 
+                let expected_event_signature = keccak256("Withdraw(uint256,address,uint256)");
+
                 for log in logs {
-                    println!("{:#?}", log);
+                    let log_topics = &log.inner.data.topics();
+                    let log_data = &log.inner.data.data;
+                    println!("Data: {:#?}", log_data);
+                    println!("Topics: {:#?}", log_topics);
+
+                    if log_topics.len() > 0
+                        && log_topics[0].as_slice() == expected_event_signature.as_slice()
+                    {
+                        // Decode id (topics[1])
+                        let id = U256::from_be_slice(&log_topics[1].as_slice());
+
+                        // Decode owner address (topics[2])
+                        let owner_bytes: [u8; 20] = log_topics[2].as_slice()[12..32]
+                            .try_into()
+                            .expect("Incorrect length: expected [u8; 20]");
+                        let owner = Address::from_slice(&owner_bytes);
+
+                        let amount = U256::from_be_slice(log_data.as_ref());
+
+                        println!("Event: Withdraw");
+                        println!("ID: {:?}", id);
+                        println!("Owner: {:?}", owner);
+                        println!("Amount: {:?}", amount);
+                    }
                 }
             }
             _ => println!("The receipt is of an unexpected type."),
         }
-    } else {
-        println!("No transaction receipt available.");
     }
 
     Ok(())
 }
-
-// Some(TransactionReceipt { inner: Legacy(ReceiptWithBloom { receipt: Receipt { status: Eip658(true), cumulative_gas_used: 49954, logs: [Log { inner: Log { address: 0x5fbdb2315678afecb367f032d93f642f64180aa3, data: LogData { topics: [0x9da6493a92039daf47d1f2d7a782299c5994c6323eb1e972f69c432089ec52bf, 0x0000000000000000000000000000000000000000000000000000000000000002, 0x000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266], data: 0x000000000000000000000000000000000000000000000000000009184e72a000 } }, block_hash: Some(0x5282469631a3a1866aa397effb44a120ee5ff3be7708bb99d192261299a35aea), block_number: Some(6), block_timestamp: Some(1723637308), transaction_hash: Some(0x78fc628fdaca0fff3bc4a096f3809706935e415a98f3e6d3470dc8f63c2ff3d5), transaction_index: Some(0), log_index: Some(0), removed: false }] }, logs_bloom: 0x04000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000000000000000000040000000000000000000000000000000000000000000000000000000040000000000000100100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000100000000000000000000000000000000000000000000000040000000200000000000000000000000002000000000000000000000000000000000000000000000000000000000000000008000000000000000000000 }), transaction_hash: 0x78fc628fdaca0fff3bc4a096f3809706935e415a98f3e6d3470dc8f63c2ff3d5, transaction_index: Some(0), block_hash: Some(0x5282469631a3a1866aa397effb44a120ee5ff3be7708bb99d192261299a35aea), block_number: Some(6), gas_used: 49954, effective_gas_price: 1515761873, blob_gas_used: None, blob_gas_price: Some(1), from: 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266, to: Some(0x5fbdb2315678afecb367f032d93f642f64180aa3), contract_address: None, state_root: Some(0xff4f18aa4f9fe3e2ced2e16eec4f40f558690482678f7fbcea5bcb94ab2242cd), authorization_list: None })
